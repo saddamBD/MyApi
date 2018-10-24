5f83c3e27d3f6fe68579e85fe6d1485713d5aee7
saddam <saddam.ruet@gmail.com>
2018-10-24 15:22:58 +0600

    my initial commit

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,10 @@
-import FluentSQLite
+import FluentPostgreSQL
 import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     /// Register providers first
-    try services.register(FluentSQLiteProvider())
+   try services.register(FluentPostgreSQLProvider())
 
     /// Register routes to the router
     let router = EngineRouter.default()
@@ -18,16 +18,18 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(middlewares)
 
     // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
+    let config = PostgreSQLDatabaseConfig(hostname: "127.0.0.1", port: 5433, username:"saddam", database:"vaporDB", password: nil, transport: .cleartext)
+    
+    let postgres = PostgreSQLDatabase(config: config)
 
     /// Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    databases.add(database: postgres, as: .psql)
     services.register(databases)
 
     /// Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
+    migrations.add(model: employeeInfo.self, database: .psql)
     services.register(migrations)
 
 }
